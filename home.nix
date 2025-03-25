39c6c6acd8432673a602da8f5f1265960e5de8f1
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-03-25 20:30:28 +0000

    additional hyprland config

--- home.nix
+++ home.nix
@@ -36,9 +36,28 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      "$mod" = "SUPER";
       exec-once = [
         "kitty"
+        "[workspace 1 silent] zen"
+      ];
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, Q, exec, kitty"
+        "$mod, X, killactive"
+        "$mod, F, fullscreen"
+        "$mod, V, togglefloating"
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, J, movewindow, l"
+        "$mod, K, movewindow, r"
+        "$mod, u, workspace, 1"
+        "$mod, i, workspace, 2"
+        "$mod, o, workspace, 3"
+        "$mod, p, workspace, 4"
+        "$mod SHIFT, u, movetoworkspace, 1"
+        "$mod SHIFT, i, movetoworkspace, 2"
+        "$mod SHIFT, o, movetoworkspace, 3"
+        "$mod SHIFT, p, movetoworkspace, 4"
       ];
     };
   };
@@ -46,4 +65,3 @@
   # Optional, hint Electron apps to use Wayland:
   # home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
-
