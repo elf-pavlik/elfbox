473bf1c2626e91ec07d9a7f93f63513144cfefc0
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-06-29 22:52:38 -0600

    switch flake to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
