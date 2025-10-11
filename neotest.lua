20f8b1c33a567af27f38770ba88126416e0e2b49
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-10-10 18:50:43 -0600

    [nvim] delete old plugin configs

--- neotest.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{ "nvim-neotest/neotest-vitest" },
-	{ "nvim-neotest/neotest-jest" },
-	{
-		"nvim-neotest/neotest",
-		opts = { adapters = { "neotest-vitest", "neotest-jest" } },
-	},
-}
