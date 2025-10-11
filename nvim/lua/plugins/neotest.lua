d0e7baa0f5da8ba585642e8539daba4aa966e404
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-10-10 18:49:53 -0600

    [nvim] re-enable plugins

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
+  },
+  opts = {
+    adapters = {
+      ["neotest-jest"] = {},
+      ["neotest-vitest"] = {},
+    },
+  },
+}
