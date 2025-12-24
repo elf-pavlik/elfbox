29a560dad2e92e27fd9bcba7ba3c4107b40dc01e
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-12-23 20:25:13 -0600

    [nvim] adjust config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,13 +5,22 @@ return {
       -- your explorer configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      filters = {
+        dotfiles = false, -- REQUIRED
+        custom = function(entry)
+          -- hide other dot-directories if desired
+          if entry.name:sub(1, 1) == "." and entry.name ~= ".dagger" then
+            return true -- hide
+          end
+        end,
+      },
     },
     picker = {
       sources = {
         explorer = {
           -- your explorer picker configuration comes here
           -- or leave it empty to use the default settings
-          auto_close = true,
+          auto_close = false,
         },
       },
     },
