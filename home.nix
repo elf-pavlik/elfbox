25ab5994fe1bcda02a0fb172d84eca244ee6d2d0
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-12-23 20:34:40 -0600

    [tealdeer] enable

--- home.nix
+++ home.nix
@@ -238,6 +238,19 @@
     icons = "auto";
   };
 
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        compact = true;
+        use_pager = true;
+      };
+      updates = {
+        auto_update = false;
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
