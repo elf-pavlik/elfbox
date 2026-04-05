13bb08015fe093844e7add8c33bf11449f7a5ef1
elf Pavlik <elf-pavlik@hackers4peace.net>
2026-04-05 12:48:09 -0600

    [wezterm] enable

--- home.nix
+++ home.nix
@@ -132,6 +132,36 @@
       background-opacity = 0.8;
     };
   };
+  
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    extraConfig = ''
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- or, changing the font size and color scheme.
+config.font_size = 15
+config.color_scheme = 'Catppuccin Macchiato'
+config.window_background_opacity = 0.8
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
+}
+
+-- Finally, return the configuration to wezterm:
+return config
+    '';
+  };
 
   services.ssh-agent = {
     enable = true;
